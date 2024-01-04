61df520c6ce2b16c424242ca82d7cc33a377309d
Ruslan Golovinsky <ruslangolovinsky@mytona.com>
2024-01-04 08:02:07 +0400

    [cmake] Small improvements

--- cmake/prelude.cmake
+++ cmake/prelude.cmake
@@ -1,17 +1,20 @@
 
 #======================================= SETUP DIRECTORIES ========================================#
 
+set(magicube_root ${CMAKE_CURRENT_SOURCE_DIR})
+
 macro(make_directory_if_not_exists dir)
 	if (NOT IS_DIRECTORY ${dir})
 		file(MAKE_DIRECTORY ${dir})
 	endif()
 endmacro()
 
-make_directory_if_not_exists(${CMAKE_CURRENT_SOURCE_DIR}/bin)
+make_directory_if_not_exists(${magicube_root}/bin)
+make_directory_if_not_exists(${magicube_root}/bin/assets)
 
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/bin)
-set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/bin)
-set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/bin)
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${magicube_root}/bin)
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${magicube_root}/bin)
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${magicube_root}/bin)
 
 #=========================================== UTILITIES ============================================#
 
@@ -31,6 +34,7 @@ message(STATUS "Detected platform: ${magicube_platform}")
 #========================================== CXX STANDARD ==========================================#
 
 set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_C_STANDARD 11)
 
 #========================================= COMPILER FLAGS =========================================#
